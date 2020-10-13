254a463804e4a8f71f0acccb46527bd361303853
Alex Popov <mslex.exe@gmail.com>
2020-10-13 20:00:55 +0300

    Initial Commit

---
+++ Home work 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 2
+//
+//  Created by Alex Popov on 13.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
